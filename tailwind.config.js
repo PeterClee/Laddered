a59e9d646543953f08e04052387debf433d27c2c
Daniel Chalk <dchalk@adaptavist.com>
2021-02-24 20:37:00 +0000

    chore: fix tailwinconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
   // tailwind.config.js
   module.exports = {
-
-    purge: [],
- 
     purge: [
- 
       './resources/**/*.blade.php',
- 
       './resources/**/*.js',
- 
       './resources/**/*.vue',
- 
     ],
      darkMode: false, // or 'media' or 'class'
      theme: {
