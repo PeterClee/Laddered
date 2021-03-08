f5b94d53568d5ff864dd1c109d0d0311f885a053
Peter <peterakie@hotmail.com>
2021-03-08 15:51:39 +0000

    Auto stash before rebase of "origin/main"

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,3 @@
-import Vue from 'vue'
+require('./bootstrap');
 
-//Main pages
-import App from './views/app.vue'
-
-
-const app = new Vue({
-    el: '#app',
-    components: { App }
-});
+require('alpinejs');
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', 'App\Http\Controllers\PagesController@index')->where('any', '.*');
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
